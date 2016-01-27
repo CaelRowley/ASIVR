11ef5d25d082065e5cf17fc0c5a90c73f49a5399
CaelRowley <caelrowley@gmail.com>
2016-01-27 23:55:15 +0000

    Added In Game Scenes / Cleaned Up
    Added in the basic scenes needed for:
    
    - Beer Toss
    - Gun Deck
    - Maze Escape
    - Runner
    - Tactics
    - The Main Menu
    
    Also reorganized files

---
+++ ASIVR/Assets/Main Menu/Scripts/SelectGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SelectGame : MonoBehaviour {
+
+	public void SelectScene (string sceneName) {
+        SceneManager.LoadScene(sceneName);
+	}
+}
