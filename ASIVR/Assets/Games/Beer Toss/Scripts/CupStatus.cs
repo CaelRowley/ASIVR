99f52c49549b3000b6f65ca8d410f5338d004346
CaelRowley <caelrowley@gmail.com>
2016-01-31 00:06:31 +0000

    Added Drunk Camera Shake
    Added drunken camera sway to Beer Toss, Sway intensity is based on the
    number of cups the player has thrown balls into.

--- ASIVR/Assets/Games/Beer Toss/Scripts/CupStatus.cs
+++ ASIVR/Assets/Games/Beer Toss/Scripts/CupStatus.cs
@@ -4,15 +4,7 @@ using UnityEngine.SceneManagement;
 
 public class CupStatus : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
-	
-	// Update is called once per frame
 	void Update () {
-        print(GameObject.FindGameObjectsWithTag("BeerCup").Length);
-
         if (GameObject.FindGameObjectsWithTag("BeerCup").Length == 0) { SceneManager.LoadScene("MainMenu"); }
     }
 }
