f5fd35fba0676fecb7ae831e1df5953788d19128
CaelRowley <caelrowley@gmail.com>
2016-01-31 01:38:19 +0000

    Added Motion Blur for Beer Toss
    Added Motion blur to Beer Toss when the first cup has been removed.
    Also imported the Unity standard asset Effects

---
+++ ASIVR/Assets/Games/Beer Toss/Scripts/DrunkBlurEffectLeft.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var motionBlur : UnityStandardAssets.ImageEffects.MotionBlur;
+var gameObjectff : GameObject[];
+var maxCup : int;
+
+function Start () {
+	motionBlur = GameObject.Find("Main Camera Left").GetComponent(UnityStandardAssets.ImageEffects.MotionBlur);
+	motionBlur.enabled = false;
+	gameObjectff = GameObject.FindGameObjectsWithTag("BeerCup");
+	maxCup = gameObjectff.length;
+}
+
+function Update () {
+	gameObjectff = GameObject.FindGameObjectsWithTag("BeerCup");
+	if(gameObjectff.length < maxCup)
+	{
+	  motionBlur.enabled = true;
+	}
+}
