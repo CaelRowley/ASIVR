88b9a1f66f5c1672201b7a443a2bf035c4e60879
CaelRowley <caelrowley@gmail.com>
2016-03-23 15:56:36 +0000

    Updated Forcewave special

---
+++ ASIVR/Assets/Games/Gun Deck/Scripts/ScaleObject.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleObject : MonoBehaviour {
+   public float targetScaleX;
+   public float targetScaleY;
+   public float targetScaleZ;
+   public float scaleTime;
+
+   void Start() {
+      StartCoroutine(ScaleOverTime(scaleTime));
+   }
+
+   // Scales the object over the given time then destroys the object
+   IEnumerator ScaleOverTime(float time) {
+      Vector3 originalScale = gameObject.transform.localScale;
+      Vector3 targetScale = new Vector3(targetScaleX, targetScaleY, targetScaleZ);
+      float currentTime = 0.0f;
+
+      do {
+         gameObject.transform.localScale = Vector3.Lerp(originalScale, targetScale, currentTime / time);
+         currentTime += Time.deltaTime;
+         yield return null;
+      }
+      while(currentTime <= time);
+
+      Destroy(gameObject);
+   }
+}
--- ASIVR/Assets/Games/Gun Deck/Scripts/Special_ForceWave.cs
+++ ASIVR/Assets/Games/Gun Deck/Scripts/Special_ForceWave.cs
@@ -1,35 +1,54 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Special_ForceWave : MonoBehaviour {
-   public float audioSensitivity = 100.0f;
-   public float forceStrength = 10.0f;
-   public float threshold = 5.0f;
+   public float audioSensitivity;
+   public float forceStrength;
+   public float threshold;
    public GameObject audioInputGameObject;
+   public GameObject projectile;
 
+   private bool isActive = false;
    private AudioInput audioInput;
 
    void Start() {
       audioInput = (AudioInput) audioInputGameObject.GetComponent("AudioInput");
    }
 
-   // Calls CreateForceWave() if conditions are met
+   // Starts CreateForceWave coroutine if conditions are met
    void Update() {
       float averageVolume = audioInput.GetAverageVolume() * audioSensitivity;
 
-      if(averageVolume > threshold) {
-         CreateForceWave(averageVolume);
+      if(!isActive && averageVolume > threshold) {
+         isActive = true;
+         StartCoroutine("CreateForceWave", averageVolume);
       }
    }
 
    // Creates a sphere around the player and destroys all enemies it collides with
-   private void CreateForceWave(float averageVolume) {
+   private IEnumerator CreateForceWave(float averageVolume) {
+
+      // Instantiates the projectile for the force wave
+      GameObject instantiatedProjectile = Instantiate(projectile,
+                                                           transform.position,
+                                                           transform.rotation)
+                                                           as GameObject;
+
+      // Waits for the projectile animation
+      yield return new WaitForSeconds(0.5f);
+
+      // Creates the overlap sphere with a radius based on the volume of if the audio input
       float radius = averageVolume * forceStrength;
       Collider[] colliders = Physics.OverlapSphere(gameObject.transform.position, radius);
 
+      // Destroys all objects with tag "Enemy" that collide with the overlap sphere
       foreach(Collider collider in colliders) {
          if(collider.tag == "Enemy") {
             Destroy(collider.gameObject);
          }
       }
+
+      // Sets isActive to false when finished
+      isActive = false;
    }
 }
